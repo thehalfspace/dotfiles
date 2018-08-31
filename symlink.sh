604707d2617d4657ad83d6347cb413b6d55fa8ba
prithvi <prith@umich.edu>
2018-08-31 11:29:29 -0400

    flux alias

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 
-# This is for my personal bash settings. Do not use this.
+# This is for my personal bash settings.
 #ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
