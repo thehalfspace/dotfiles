f8a442077cffeaa0f565519e809ad784812e9fe3
prithvi <prith@umich.edu>
2018-11-19 11:35:32 -0500

    add zshrc

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,11 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+
+# Install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 
 # This is for my personal bash settings.
 #ln -s ~/dotfiles/.bash_profile ~/.bash_profile
